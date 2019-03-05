3197ecc7cb1c5199a8471e5e76b932c2a43f00a2
Kevin <kevin.fal360@gmail.com>
2019-03-05 13:59:47 -0500

    Added status that updates when clicking and fixed bounds + added text box

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simulator
+
+The plan is to create a Evolution simulator in which creatures adapt to the environment through generations
+
+Just run the py file as of now
--- simulator.py
+++ simulator.py
@@ -4,13 +4,38 @@ import time
 import sys
 import random
 pygame.init()
+pygame.font.init()
+
+
+#font
+font = pygame.font.SysFont('times new roman', 30, True)
 
 #window stuff
-width = 1000
-height = 800
-win = pygame.display.set_mode((width, height))
+
+windowWidth = 1500
+windowHeight = 700
+win = pygame.display.set_mode((windowWidth , windowHeight))
 
 #bounds
+tileSize = 30
+tileWidth = 30 #amount of tiles across
+tileHeight = 22 #amount of tiles height
+
+height = tileSize * tileHeight
+width = tileSize * tileWidth
+#creature bound
+boundXMin = 50
+boundYMin =50
+boundXMax = 500
+boundYMax = 500
+
+
+
+#debug String
+debug = ""
+debugging = False
+
+status = ""
 
 class creature(object):
 
@@ -24,6 +49,7 @@ class creature(object):
         self.y = random.randint(50,750)
         self.facing = random.randint(0,360)
         self.speed = 20
+
         
     #eating function
     def eat(self):
@@ -43,14 +69,11 @@ class creature(object):
 
     
     def move(self):
-        speed = 20 #increase this variable slows the character down
-
-        #random
-        
-        chance = random.randint(0,100)
+        speed = 10 #increase this variable slows the character down
        
         
-        if self.x in range(50,width-50) and self.y in range(50,height-50):
+        #if the creature is in these bounds
+        if self.x in range(50,width) and self.y in range(50,height):
             
 
             
@@ -76,7 +99,27 @@ class creature(object):
             self.y = int(self.y)
             
         else:
-            self.facing = abs(self.facing - 180)
+            #if out of bounds in x 
+            if not(self.x in range(50,width)):
+
+                #if out of bounds to the right
+                if(self.x >= width):
+                    self.facing = abs(self.facing - 180)
+                    #if out of bounds to the left
+                elif(self.x <= 0):
+                    self.facing = abs(self.facing + 180) -360
+            
+                
+                #if out of bounds y 
+            if not(self.y in range(50,height)):
+
+                #if out of bounds y
+                if(self.y >= height - 50):
+                    self.facing = abs(self.facing + 180) - 360
+                elif(self.y <= 50):
+                    self.facing = abs(self.facing - 180)
+                
+            
             self.facing += random.randrange(-5, 5)
             if (self.facing > 360):
                 self.facing -= 360
@@ -112,7 +155,7 @@ class creature(object):
     def choice(self):
 
         chance = random.randint(0,100)
-        if chance >= 50:
+        if chance == 50:
             self.eat()
         else:
             self.move()
@@ -141,6 +184,9 @@ class omnivore(creature):
 class herbivore(creature):
     def __init__(self):
         pass
+
+
+#tile class, what makes up the whole map
 class tile(object):
     def __init__(self,x,y):
         self.id = 0
@@ -161,12 +207,80 @@ class tile(object):
             return True
 
 
+
+class textBox(object):
+ 
+    def __init__(self,height,width,xy):
+
+        
+
+        self.height = height
+        self.width = width
+        
+        self.x = windowWidth - (self.width + 25) # to just place it in the reference of the window width
+        self.y = 50
+        self.mainText = text((500,500) ,"Status")
+        self.texts = [] #list of text that the box will display
+        
+
+    def draw(self):
+        #rect(Surface, color, Rect, width=0)
+        #Draws a rectangular shape on the Surface. The given Rect is the area of the rectangle.
+        #The width argument is the thickness to draw the outer edge. If width is zero then the rectangle will be filled
+
+        #this rectangle is the background for the text
+
+
+        for x in self.texts:
+            x.draw()
+        self.mainText.draw()
+        pygame.draw.rect(win,(255,255,255), (self.x, self.y,self.width , self.height))
+        
+class text():
+    def __init__(self,xy,txt):
+
+       
+        
+        self.x = xy[0]
+        self.y = xy[1]
+        self.txt = txt
+    def draw(self):
+        text = font.render((self.txt), 1, (0,0,255))
+        win.blit(text,(self.x,self.y))
+    
+class mainBox(textBox):
+       #this constructor is for the main text box
+    def __init__(self):
+        
+        
+        self.height = 620
+        self.width = 520
+        self.texts = [] #list of text that the box will display
+        self.x = windowWidth - (self.width + 25) # to just place it in the reference of the window width
+        self.y = 50
+        self.mainText = text((self.x,self.y) ,"Status: " + status)
+    def draw(self):
+
+        self.mainText = text((self.x,self.y) ,"Status: " + status)
+    #rect(Surface, color, Rect, width=0)
+    #Draws a rectangular shape on the Surface. The given Rect is the area of the rectangle.
+    #The width argument is the thickness to draw the outer edge. If width is zero then the rectangle will be filled
+
+    #this rectangle is the background for the text
+        pygame.draw.rect(win,(255,255,255), (self.x, self.y,self.width , self.height))
+        for x in self.texts:
+            x.draw()
+        self.mainText.draw()
+
+
+
+
+
+
 #utility functions
 
 
 def onTile(pos):#accepts tuple as argument, returns tile
-    posx = pos[0]
-    posy = pos[1]
 
     tile = -1
 
@@ -186,19 +300,21 @@ clock = pygame.time.Clock()
 
 run = True
 
+#collections of objects 
+#i.e world has everything, map has tiles, organisms has living things ect
 world = []
 map = []
 organisms = []
 creatures = []
 
-
+textBoxes = []
 
 def initWorld():
 
 
     #generates the tile map
-    for y in range(0,height):
-        for x in range(0,width):
+    for y in range(0,tileSize*tileHeight):#I want 30 tiles accross, and tiles are 30 wide so 30 * 30
+        for x in range(0,tileSize * tileWidth): #same as above but i was 25 tiles going down
             if (x%31==0 and y%31==0):
                 newTile = tile(x,y)
                 newTile.nutrivalue = random.randint(0,255)
@@ -206,31 +322,41 @@ def initWorld():
 
                 map.append(newTile)
                 world.append(newTile)
-    
+
+    #generates creatures
     for i in range(0,5):
         newCreature = creature()
-        creature.x = random.randint(50,width-50)
-        creature.y = random.randint(50,height-50)
+        creature.x = random.randint(boundXMin,boundXMax)
+        creature.y = random.randint(boundYMin,boundYMax)
+
         creatures.append(newCreature)
         world.append(newCreature)
         organisms.append(newCreature)
+    
+
+    box = mainBox()
+    world.append(box)
 
 
-
+#creates the world
 initWorld()
 
 def redrawGameWindow():
 
+    
     for x in world:
         x.draw()
 
 
+    
+    if debugging:
+        
+        debugText = font.render(debug,1,(0,0,255))
+        win.blit(text, (500,500))
+
+
     pygame.display.update() #updates window
 
-    
-
-
-c0 = creature()
 
 
 
@@ -242,15 +368,22 @@ while(run):
 
 
     for event in pygame.event.get():
+
+        #on click
         if event.type == pygame.MOUSEBUTTONUP:
+            
+            #checks if the click is on a tile
+            
             pos = pygame.mouse.get_pos()
             for x in map:#loops through all of the tiles in the map
                 if(x.getpos(pos)):#if the click is on a tile
-                    print(x.nutrivalue)#print the tile's nutritional value
+                    status = "Nutrivalue: " +str(x.nutrivalue)#print the tile's nutritional value
+
+            #end tile check
 
 
 
-
+        #if the user clicks exit
         if event.type == pygame.QUIT:
             #this is so you can actually close the window
             run = False #breaks loop
